a5cc1d98b55a5c1c5951852df4ae175045db97b8
Lin-Kangjing <32667898+Lin-Kangjing@users.noreply.github.com>
2021-07-08 07:52:05 +0000

    Deploying to gh-gapes from  @ a6ceac5320f280e7f8ea4f5b37c42067dfda6f2d 🚀

---
+++ precache-manifest.3f3b66a76a3f35b084aacaea2fe8a083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576043e557ccb0029f227fd0f213037c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7008a27468ee7aa7413",
+    "url": "/static/css/2.cf167d75.chunk.css"
+  },
+  {
+    "revision": "d29cd8b30c25a04885c1",
+    "url": "/static/css/main.47a3d212.chunk.css"
+  },
+  {
+    "revision": "f7008a27468ee7aa7413",
+    "url": "/static/js/2.04ac339e.chunk.js"
+  },
+  {
+    "revision": "d29cd8b30c25a04885c1",
+    "url": "/static/js/main.736f39f4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
