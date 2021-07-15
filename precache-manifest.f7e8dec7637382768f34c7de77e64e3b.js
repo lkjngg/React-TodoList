e341f8048d25a7a7657294c5d2a82aef3330920b
Lin-Kangjing <32667898+Lin-Kangjing@users.noreply.github.com>
2021-07-15 03:20:02 +0000

    Deploying to tencent from @ Lin-Kangjing/React-TodoList@df192e185319729f03db49888c886c708d5e97c4 🚀

---
+++ precache-manifest.f7e8dec7637382768f34c7de77e64e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d41621675c8d881b1e1d508253567cd",
+    "url": "/React-TodoList/index.html"
+  },
+  {
+    "revision": "8c44da493887cdca3984",
+    "url": "/React-TodoList/static/css/2.cf167d75.chunk.css"
+  },
+  {
+    "revision": "d29cd8b30c25a04885c1",
+    "url": "/React-TodoList/static/css/main.47a3d212.chunk.css"
+  },
+  {
+    "revision": "8c44da493887cdca3984",
+    "url": "/React-TodoList/static/js/2.8415bcab.chunk.js"
+  },
+  {
+    "revision": "d29cd8b30c25a04885c1",
+    "url": "/React-TodoList/static/js/main.736f39f4.chunk.js"
+  },
+  {
+    "revision": "108dbe416174f98bd927",
+    "url": "/React-TodoList/static/js/runtime~main.9330353c.js"
+  }
+]);
